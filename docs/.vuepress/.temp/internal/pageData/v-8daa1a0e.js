b070d3bebefee3049c50994c58ed3799668549de
juhanjakob <juhanjakob.kalberg@tptlive.ee>
2021-03-10 11:41:15 +0200

    first commit

---
+++ docs/.vuepress/.temp/internal/pageData/v-8daa1a0e.js
@@ -0,0 +1,21 @@
+export const data = {
+  "key": "v-8daa1a0e",
+  "path": "/",
+  "title": "Õpidokud",
+  "lang": "et",
+  "frontmatter": {
+    "lang": "et",
+    "title": "Õpidokud",
+    "description": "Õpilaste tööde dokumentatsioon",
+    "home": true
+  },
+  "excerpt": "",
+  "headers": [],
+  "filePathRelative": "README.md",
+  "git": {}
+}
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  __VUE_HMR_RUNTIME__.updatePageData(data)
+}
